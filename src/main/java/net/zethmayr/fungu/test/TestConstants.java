7a7089ca65b1357bbc2e7175d121d7e4ff083aa8
Benjamin Zethmayr <bzethmayr@users.noreply.github.com>
2023-05-01 13:34:53 -0500

    More build stuff (#1)
    * Identifies license and updates POM metadata
    
    * Fixes example syntax, adds javadoc

--- src/main/java/net/zethmayr/fungu/test/TestConstants.java
+++ src/main/java/net/zethmayr/fungu/test/TestConstants.java
@@ -6,23 +6,48 @@ import java.util.UUID;
 
 import static net.zethmayr.fungu.test.TestExceptionFactory.becauseConstantsOnly;
 
+/**
+ * Declares constants of frequent use in testing.
+ */
 public class TestConstants {
 
     private TestConstants() {
         throw becauseConstantsOnly();
     }
 
+    /**
+     * An expected value.
+     */
     public static final String EXPECTED = "expected";
 
+    /**
+     * An unexpected value.
+     */
     public static final String UNEXPECTED = "un" + EXPECTED;
 
+    /**
+     * An unpredictable value.
+     */
     public static final String SHIBBOLETH = UUID.randomUUID().toString();
 
+    /**
+     * A halfway-decent random number generator,
+     * probably not correlated to that used by the code under test.
+     */
     public static final Random TEST_RANDOM = new SecureRandom();
 
+    /**
+     * A value that will not be created by any application code.
+     */
     public static final Object NOT_EVEN_WRONG = new Object();
 
+    /**
+     * A null object without the IDE getting worried.
+     */
     public static Object NULL_OBJECT = null;
 
+    /**
+     * A null string without the IDE getting worried.
+     */
     public static final String NULL_STRING = null;
 }
--- src/main/java/net/zethmayr/fungu/test/TestHelper.java
+++ src/main/java/net/zethmayr/fungu/test/TestHelper.java
@@ -5,11 +5,24 @@ import java.lang.reflect.InvocationTargetException;
 
 import static net.zethmayr.fungu.test.TestExceptionFactory.becauseStaticsOnly;
 
+/**
+ * Static methods used in very common test cases.
+ */
 public final class TestHelper {
     private TestHelper() {
         throw becauseStaticsOnly();
     }
 
+    /**
+     * Invokes a class's no-argument constructor, if any,
+     * rethrowing any exception thrown by the constructor
+     * or during invocation.
+     *
+     * @param underTest a class under test
+     * @param <T>       the type under test
+     * @return any instance created
+     * @throws Exception as thrown in instantiation
+     */
     public static <T> T invokeDefaultConstructor(
             final Class<T> underTest
     ) throws Exception {
