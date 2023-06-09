4b27a937d1d23d4652cace899a9d24509cadaa77
Benjamin Zethmayr <bzethmayr@users.noreply.github.com>
2023-06-08 21:08:52 -0500

    Random string (#8)
    * Adds randomString and variants
    
    * Fix bounds check vs 3-4 byte codepoints

--- src/main/java/net/zethmayr/fungu/test/TestHelper.java
+++ src/main/java/net/zethmayr/fungu/test/TestHelper.java
@@ -1,8 +1,13 @@
 package net.zethmayr.fungu.test;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
 
+import static net.zethmayr.fungu.test.TestConstants.TEST_RANDOM;
+import static net.zethmayr.fungu.test.TestExceptionFactory.becauseIllegal;
 import static net.zethmayr.fungu.test.TestExceptionFactory.becauseStaticsOnly;
 
 /**
@@ -23,9 +28,7 @@ public final class TestHelper {
      * @return any instance created
      * @throws Exception as thrown in instantiation
      */
-    public static <T> T invokeDefaultConstructor(
-            final Class<T> underTest
-    ) throws Exception {
+    public static <T> T invokeDefaultConstructor(final Class<T> underTest) throws Exception {
         final Constructor<T> defaultConstructor = underTest.getDeclaredConstructor();
         defaultConstructor.setAccessible(true);
         try {
@@ -33,5 +36,76 @@ public final class TestHelper {
         } catch (final InvocationTargetException ite) {
             throw (Exception) ite.getTargetException();
         }
+    }
+
+    /**
+     * Returns the first and only codepoint in the string, or throws.
+     *
+     * @param bound     a bounding string expected to contain one codepoint.
+     * @param boundName what sort of bound this is.
+     * @return the first and only codepoint
+     * @throws IllegalArgumentException if the string does not represent a codepoint.
+     */
+    private static int checkedBound(@NotNull final String bound, final String boundName) {
+        return Optional.of(bound)
+                .filter(s -> s.length() == 1)
+                .map(String::codePoints)
+                .orElseThrow(() -> becauseIllegal("'%s' is not a valid %s bound", bound, boundName))
+                .findFirst()
+                .orElseThrow(IllegalStateException::new); // unreachable
+    }
+
+    /**
+     * Returns a random string with the given length in codepoints,
+     * containing characters within the given bounds.
+     *
+     * @param length the required length
+     * @param lower  the minimum single character string.
+     * @param upper  the maximum single character string.
+     * @return a random string.
+     */
+    public static String randomString(final int length, final String lower, final String upper) {
+        return randomString(length, checkedBound(lower, "lower"), checkedBound(upper, "upper"));
+    }
+
+    /**
+     * Returns a random string with the given length in codepoints,
+     * containing characters within the given bounds.
+     *
+     * @param length the required length.
+     * @param lower  the minimum codepoint value (inclusive).
+     * @param upper  the maximum codepoint value (inclusive).
+     * @return a random string.
+     */
+    public static String randomString(final int length, final int lower, final int upper) {
+        if (length < 1) {
+            throw becauseIllegal("%s is not a valid length", length);
+        }
+        if (upper > Character.MAX_CODE_POINT) {
+            throw becauseIllegal("Upper bound %s exceeds codepoint range", upper);
+        }
+        if (lower < 0) {
+            throw becauseIllegal("Lower bound %s is less than zero", lower);
+        }
+        if (upper < lower) {
+            throw becauseIllegal("Upper bound %s is less than lower bound %s", upper, lower);
+        }
+        final StringBuilder buffer = new StringBuilder();
+        TEST_RANDOM.ints(lower, upper + 1)
+                .sequential()
+                .limit(length)
+                .forEach(buffer::appendCodePoint);
+        return buffer.toString();
+    }
+
+    /**
+     * Returns a random string of the given length,
+     * excluding legacy control characters and 3-4 byte characters.
+     *
+     * @param length the required length.
+     * @return a random string.
+     */
+    public static String randomString(final int length) {
+        return randomString(length, 32, Character.MAX_VALUE);
     }
 }
